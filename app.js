208ccf88b26065b26707bb06967dc9f7b398d1b6
Julian Garcia <69781917+julianagar@users.noreply.github.com>
2022-08-13 21:31:46 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const request = require('request')
+const https = require("https");
+const bodyParser = require("body-parser")
+const ejs = require("ejs");
+
+const app = express()
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/", function(req, res) {
+  const url = "https://api.kanye.rest/"
+
+  https.get(url, function(response) {
+    response.on("data", function(data) {
+      const response = JSON.parse(data)
+      const quote = response.quote
+
+      res.render("quote", {response: quote})
+
+      // res.write('<h1>Kanye says </h1>"' + response.quote + '"')
+      // res.write('<br> ')
+
+      res.send()
+
+    })
+  })
+})
+app.listen(process.env.PORT || 3000, function() {
+  console.log("im here!")
+})
