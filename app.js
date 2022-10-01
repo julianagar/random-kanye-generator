ef3a9e1325019c3d927b889914dc6d30c8a1ec16
Julian Garcia <69781917+julianagar@users.noreply.github.com>
2022-10-01 15:09:11 -0700

    changed ports

--- app.js
+++ app.js
@@ -28,7 +28,6 @@ app.post("/", function(req, res) {
     })
   })
 })
-
-app.listen(process.env.PORT || 3000, function() {
+app.listen(3000, '0.0.0.0'), function() {
   console.log("im here!")
-})
+}
